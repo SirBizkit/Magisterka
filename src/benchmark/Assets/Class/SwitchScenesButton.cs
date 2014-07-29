3adbb972dc0e179fa344563157238d1e10a4c702
Bizkit000 <bizkit2@o2.pl>
2014-07-29 21:13:51 +0200

    Zmieniono sposob przelaczania sceny by uniezaleznic program od zmieniajacej sie kolejnosci, dodano test sprawdzajacy FPS w zaleznosci od wzrastajacej ilosci obiektow na ekranie, dodano wyniki testow z pierwszych dwoch 2D i 3D

--- src/benchmark/Assets/Class/SwitchScenesButton.cs
+++ src/benchmark/Assets/Class/SwitchScenesButton.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class SwitchScenesButton : MonoBehaviour {
 
-	public int levelToLoad = 0;
+	public string levelToLoad = "MainScene";
 	
 	void OnGUI () {
 		if(GUI.Button(new Rect(Screen.width/2+10,10+10,Screen.width/8+10,Screen.height/8+10), "SwitchLevel")) {
---
+++ src/benchmark/Assets/Class/TestIncrementingObjects.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestIncrementingObjects : TestMain {
+	
+	public override void Start () {
+		Application.targetFrameRate = 60;
+		this.guiText.text = "";
+		EventManager.instance.AddListener(this as IEventListener, "BenchmarkEvent");
+		
+		// Test ilosci obiektow na ekranie
+		
+		addTest ("Benchmark 50", 50, true, 0);
+		addTest ("Benchmark 100", 100, true, 0);
+		addTest ("Benchmark 250", 250, true, 0);
+		addTest ("Benchmark 500", 500, true, 0);
+		addTest ("Benchmark 750", 750, true, 0);
+		addTest ("Benchmark 1000", 1000, true, 0);
+		addTest ("Benchmark 1250", 1250, true, 0);
+		addTest ("Benchmark 1500", 1500, true, 0);
+		addTest ("Benchmark 1750", 1750, true, 0);
+		addTest ("Benchmark 2000", 2000, true, 0);
+	}
+}
